a5f485c5dc86821b41c0fa62ed38f7d8569182c4
aldian <69229914+aldianwijanarko@users.noreply.github.com>
2020-08-13 22:11:19 +0700

    pert 11 : membuat template

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index')->middleware(['auth', 'admin'])->name('home');
+
+Route::get('/student', 'StudentController@index')->middleware(['auth', 'admin'])->name('student');
+Route::get('/student/create', 'StudentController@create')->middleware(['auth', 'admin'])->name('student_create');
+Route::post('/student', 'StudentController@store');
+Route::delete('/student/{id}', 'StudentController@destroy');
+Route::get('/student/{id}/edit', 'StudentController@edit');
+Route::patch('/student/{student}', 'StudentController@update');
+
+Route::get('/teacher', 'TeacherController@index');
+Route::get('/baak', 'BaakController@index');
+Route::get('/user', 'UserController@index');
+Route::get('/profil', 'ProfilController@index')->name('profil');
+
+Route::get('/hallo', function () {
+    return 'Hello World!';
 });
 
-Route::get('mhs', 'mahasiswacontroller@index');
+Auth::routes();
