2a1ee111ee975eeb571c77aaf3f05d958a9e7d49
aldian <69229914+aldianwijanarko@users.noreply.github.com>
2020-08-13 20:18:21 +0700

    Add files via upload

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,6 @@
+<h4>Main Menu</h4>
+<div class="list-group list-group-flush">
+  <a href="#" class="list-group-item list-group-item-action">MAHASISWA</a>
+  <a href="#" class="list-group-item list-group-item-action">DOSEN</a>
+
+</div>
